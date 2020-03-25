de2728be090d48989db7f4ebc6ffdc39bf4b1b7e
jabrecer <jabrecer@yahoo.com>
2020-03-24 22:47:00 -0700

    first working version

--- assets/js/telemed.js
+++ assets/js/telemed.js
@@ -82,33 +82,60 @@ var app = angular.module('telemed', [
 
     .controller('telemed-ctrl', function (
         //$scope, $rootScope, $stateParams, $state, $timeout, WebAPI, $document, $localStorage, $filter) {
-        $scope, $rootScope, ArAPI) {
+        $scope, $rootScope, ArAPI, $filter) {
         var sys = $rootScope.sys;
 
         $scope.data = {};
         var data = $scope.data;
 
-        $scope.reset = function () {
-            data.cust_id = '';
+        $scope.clear = function () {
             data.cellLast4 = '';
-            data.plan = null;
             data.paid = false;
             data.verifyByCode = true;
             data.codeSent = false;
             data.codeVerified = false;
             data.pinCode = '';
+            data.last4ssn = '';
+            data.dob = null;
+            data.mmn = '';
+        };
+
+
+        $scope.reset = function () {
+            data.plan = null;
+            data.price = 0;
+            data.cust_id = '';
+
+            $scope.clear();
         };
 
         $scope.reset();
 
-        $scope.onChange = function (field) {
+        $scope.selectPlan = function () {
+            if (data.plan === 'M') {
+                data.price = 40;
+            }
+            else if (data.plan === '1') {
+                data.price = 359;
+            }
+            else if (data.plan === '2') {
+                data.price = 599;
+            }
+            else {
+                data.price = 0;
+            }
+
+            return;
+        }
+
+        $scope.onChange = function (tag) {
             var x;
 
             if (data.curacao) {
                 delete data.curacao;
             }
 
-            if (field === 'cust_id' && data.cust_id) {
+            if (tag === 'cust_id' && data.cust_id) {
                 x = data.cust_id.replace(/[^0-9\-]/g, '');
 
                 if (x.length > 8) {
@@ -117,6 +144,15 @@ var app = angular.module('telemed', [
 
                 data.cust_id = x;
 
+            }
+            else if ('pinCode,last4ssn'.indexOf(tag) !== -1 && data[tag]) {
+                x = data[tag].replace(/[^0-9\-]/g, '');
+
+                if (x.length > 4) {
+                    x = x.slice(0, 4);
+                }
+
+                data[tag] = x;
             }
         };
 
@@ -133,7 +169,7 @@ var app = angular.module('telemed', [
                     && data.curacao[tag.toUpperCase()].toString().length > 0
                     && data.curacao.EMAIL.toUpperCase() === data.curacao.EMAIL2.toUpperCase();
             }
-            else if (tag === 'dob' && data.curacao) {
+            else if (tag === 'dob2' && data.curacao) {
                 age = moment().diff(moment($filter('date')(new Date(data.curacao.DOB), 'yyyyMMdd'), 'YYYYMMDD'), 'years');
 
                 return age >= 18;
@@ -148,14 +184,59 @@ var app = angular.module('telemed', [
             else if (tag === 'zip' && data.curacao) {
                 return data.curacao.ZIP && data.curacao.ZIP.length === 5;
             }
+            else if ('pinCode,last4ssn'.indexOf(tag) !== -1 && data[tag]) {
+                re = /^\d{4}$/;
+                return re.test(data[tag]);
+            }
+            else if (tag === 'dob' && data[tag]) {
+                console.log(data[tag])
+                age = moment().diff(moment($filter('date')(new Date(data[tag]), 'yyyyMMdd'), 'YYYYMMDD'), 'years');
+                return age >= 18;
+            }
+            else if (tag === 'mmn' && data[tag]) {
+                return data[tag].length > 0;
+            }
+            else if (tag.indexOf('password') !== -1) {
+                re = /^.*(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/;
+                return re.test(data[tag]);
+            }
             else if (data.curacao) {
                 return data.curacao[tag.toUpperCase()] && data.curacao[tag.toUpperCase()].toString().length > 0;
 
             }
 
+            return false;
         };
 
-        $scope.continueWithPlan = function (cust_id) {
+
+        $scope.step2ok = function () {
+            return $scope.step2buttonOk()
+                && data.curacao
+                ;
+        };
+
+        $scope.confirmButtonOk = function () {
+            return data.curacao
+                && $scope.isOk('f_name')
+                && $scope.isOk('l_name')
+                && $scope.isOk('email')
+                && $scope.isOk('email2')
+                && $scope.isOk('gender')
+                && $scope.isOk('dob2')
+                && $scope.isOk('cell')
+                && $scope.isOk('street')
+                && $scope.isOk('city')
+                && $scope.isOk('state')
+                && $scope.isOk('zip')
+                && $scope.isOk('password')
+                && $scope.isOk('password2')
+                && data.password === data.password2
+                ;
+        };
+
+        $scope.continueWithPlan = function () {
+            $scope.clear();
+
             sys.ajax(
                 ArAPI.pullTeleMed,
                 {
@@ -227,7 +308,10 @@ var app = angular.module('telemed', [
                 ArAPI.pullTeleMed,
                 {
                     cust_id: data.cust_id,
-                    pinCode: data.pinCode
+                    pinCode: data.pinCode,
+                    ssn: data.last4ssn,
+                    dob: data.dob ? $filter('date')(new Date(data.dob), 'yyyy-MM-dd') : null,
+                    mmaiden: data.mmn
                 },
                 function (obj) {
                     if (obj.OK) {
@@ -267,6 +351,47 @@ var app = angular.module('telemed', [
             );
         };
 
+        //pushTeleMed
+        $scope.pushTeleMed = function () {
+            data.curacao.PLAN = data.plan;
+            data.curacao.pass = data.password;
+            sys.ajax(
+                ArAPI.pushTeleMed,
+                data.curacao,
+                function (obj) {
+                    if (obj.OK) {
+                        document.activeElement.blur();
+                        console.log(obj);
+                        data.paid = true;
+
+                        Swal.fire({
+                            title: '<strong class="text-primary">Thank you for your order!</strong>',
+                            icon: 'success',
+                            html:
+                                'Your confirmation number is: <b>' + obj.INFO + '_' + obj.INV_NO + '</b><br /><br />' +
+                                '<p class="fs-1">An email from Hello Alvin containing a link with setup instructions was sent to:</p>'
+                                + '<a class="fs-1" href="mailto:' + data.curacao.EMAIL + '">' + data.curacao.EMAIL + '</a>',
+                            showCloseButton: true,
+                            confirmButtonText:
+                                '<i class="fa fa-thumbs-up"></i> Great!',
+                        }).then(() => {
+                            $rootScope.$apply(function () {
+                                $scope.reset();
+                                window.scrollTo(0, 0);
+                            });
+
+                        })
+
+                    }
+                    else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: sys.toTitleCase(obj.INFO, 2),
+                        })
+                    }
+                },
+            );
+        };
 
     })
     .value('version', '0.1');
